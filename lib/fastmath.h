630fc3b040fdf5501a827f64cbcdaf4796618077
Dieter Steiner <spoilerhead@gmail.com>
2011-12-15 17:08:05 +0100

    fastsqrt2 function added

--- lib/fastmath.h
+++ lib/fastmath.h
@@ -102,4 +102,23 @@ float FASTMATH_LOCAL fastsqrt( const float fg)
     return n;
 }
 
+float FASTMATH_LOCAL fastsqrt2(const float x)  
+{
+  union
+  {
+    int i;
+    float x;
+  } u;
+  u.x = x;
+  u.i = (1<<29) + (u.i >> 1) - (1<<22); 
+  
+  // Two Babylonian Steps (simplified from:)
+  // u.x = 0.5f * (u.x + x/u.x);
+  // u.x = 0.5f * (u.x + x/u.x);
+  u.x =       u.x + x/u.x;
+  u.x = 0.25f*u.x + x/u.x;
+
+  return u.x;
+}
+
 #endif //FASTMATH_H
