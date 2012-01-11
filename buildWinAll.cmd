f139dd67b9f6b0a773f92b4bf0e29a905a532b2e
Dieter Steiner <spoilerhead@gmail.com>
2012-01-11 08:14:04 +0100

    added build all script for windows

---
+++ buildWinAll.cmd
@@ -0,0 +1,20 @@
+call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+cd FatToni
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+cd ..\Gelatin
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+cd ..\Grain
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+cd ..\SiliconBonk
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+cd ..\WaveletDenoise
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+cd ..\WaveletSharpen
+call buildwin
+msbuild /t:Rebuild /p:Configuration=Release
+pause
