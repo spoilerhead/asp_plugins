8a0a4f972861a925c13d408038a3c1bd576c08af
Dieter Steiner <spoilerhead@gmail.com>
2012-02-02 14:04:44 +0100

    added working 16 bit modes, need some more testing

--- lib/blend_modes.h
+++ lib/blend_modes.h
@@ -6,6 +6,8 @@
  * (C) 2010
  * licensed: GPL v2+
  */
+ 
+ #include <colorspace.h>
 
 
 //http://www.linuxtopia.org/online_books/graphics_tools/gimp_advanced_guide/gimp_guide_node55_002.html
@@ -33,17 +35,16 @@
 
 //16 bit routines
 
-#define MULTIPLY16(f,b) (((f) * (b))>>16)
-#define SCREEN16(fo,b) ( 0xffff-MULTIPLY16((0xffff-(fo)),(0xffff- (b))) )
-//overlay maybe broken?
-//#define OVERLAY16(fo,b) ( MULTIPLY16((b) , SCREEN16((fo),(b))) + MULTIPLY16((0xffff - (b)) , MULTIPLY16((fo),(b))) )
-
-//#define OVERLAY16(fo,b) (((b < 0x8000) ? (2 * fo * b / 0xffff):(0xffff - 2 * (0xffff - fo) * (0xffff - b) / 0xffff)))
-
-
-
+#define MULTIPLY16(f,b) ((((unsigned int)f) * (b))>>16)
+#define SCREEN16(fo,b) ( WHITEi-MULTIPLY16((WHITEi-(fo)),(WHITEi- (b))) )
+#define OVERLAY16(fo,b) ( MULTIPLY16((b) , SCREEN16((fo),(b))) + MULTIPLY16((WHITEi - (b)) , MULTIPLY16((fo),(b))) )
 #define BLEND16(fo,b,o) (MULTIPLY16((fo),(o)) + MULTIPLY16((b),(0xFFFF-(o))))
 
-#define MIX16(fo,b) (((b)+(fo)-0x8000))
+#define SOFTLIGHT16(fo,b) ( MULTIPLY16((WHITEi-(b)),MULTIPLY16((fo),(b))) + MULTIPLY16((b),SCREEN16((fo),(b))) )
+#define HARDLIGHT16(fo,b) ((fo>0x8000)?  (WHITEi- MULTIPLY16((WHITEi-2*((fo)-0x8000)),(WHITEi-(b))))  :  (2*MULTIPLY16((fo),(b)))   )
+
+#define SHADOW_RECOVERY16(img,q) (clampI(MULTIPLY16(SOFTLIGHT16(WHITEi-(img),(img)),(q))+MULTIPLY16((WHITEi-(q)),(img))));
+
+#define MIX16(fo,b) (  clampI(((unsigned int)b)+(fo)-0x8000)  )
 
 #endif
